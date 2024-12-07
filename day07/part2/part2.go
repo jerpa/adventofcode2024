5e13315699040bbc96fd03d6c42bb3d6d0fdfe83
Stefan Samuelsson <stefan.samuelsson@entire.se>
2024-12-07 06:16:03 +0100

    day07

---
+++ day07/part2/part2.go
@@ -0,0 +1,37 @@
+package part2
+
+import (
+	"fmt"
+	c "github.com/jerpa/adventofcode2024/common"
+	"strings"
+)
+
+func Part2(data []string) int {
+	sum := 0
+	for _, v := range data {
+		s := strings.Split(v, ": ")
+		target := c.GetInt(s[0])
+		nums := c.GetInts(strings.Split(s[1], " "))
+		if calc(nums, 0, target) {
+			sum += target
+		}
+
+	}
+
+	return sum
+}
+func calc(nums []int, sum, target int) bool {
+	if len(nums) == 0 {
+		return sum == target
+	}
+	if calc(nums[1:], sum+nums[0], target) {
+		return true
+	}
+	if calc(nums[1:], sum*nums[0], target) {
+		return true
+	}
+	if calc(nums[1:], c.GetInt(fmt.Sprintf("%d%d", sum, nums[0])), target) {
+		return true
+	}
+	return false
+}
